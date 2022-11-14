8df9362bbff7aaa4deb8754210145d151426530d
Anuj Saraogi <saraogi.anuj9@gmail.com>
2022-11-14 18:38:56 +0530

    Added Some CSS styling

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './Headline.css';
 
+import { headlineNews } from '../utils/News';
+
 const Headline = () => {
-  return <div>Headline</div>;
+  return (
+    <div className="main-container">
+      <div className="image-container">
+        <img src={headlineNews?.image} alt="News" />
+      </div>
+      <div className="details-container">
+        <div>
+          <h1>{headlineNews?.title}</h1>
+        </div>
+        <div>
+          <p>{headlineNews?.p}</p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Headline;
